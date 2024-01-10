01947555fc075b8b1be4138fe75e4030d4f740d4
Emily Cheng <echeng0123@gmail.com>
2024-01-10 13:32:58 -0500

    setting up individual card pages

---
+++ client/src/components/AlbumCard.jsx
@@ -0,0 +1,32 @@
+export default function AlbumCard() {
+	return (
+		<section id="album-card-container">
+			<div id="album-info-review">
+				<div id="album-info">
+					<img src="" alt="album art" />
+					<div>
+						<p>Album name</p>
+						<p>Year released</p>
+						<p>By Artist name</p>
+					</div>
+				</div>
+				<div id="album-review">
+					<h3>Your review here, if it exists.</h3>
+				</div>
+			</div>
+			<div id="album-icons">
+				<div>
+					<p>listen</p>
+					<p>favorite</p>
+					<p>to listen</p>
+				</div>
+				<div>
+					<p>RATE/RATING HERE</p>
+				</div>
+				<div>
+					<p>Add new review</p>
+				</div>
+			</div>
+		</section>
+	);
+}
