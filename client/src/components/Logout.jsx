09c6abf19b6c64ce9bc0b3f55a92ce7760f421c5
Emily Cheng <echeng0123@gmail.com>
2024-01-11 17:15:10 -0500

    completed logout functionality

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,7 +1,18 @@
-export default function Logout() {
-	return (
-		<section>
-			<h1>Logout here</h1>
-		</section>
-	);
+// This component handles user logoff.
+
+import { useEffect } from "react";
+import { logout } from "../../fetching/local";
+import { useNavigate } from "react-router-dom";
+
+export default function Logout({ token, setToken }) {
+	const nav = useNavigate();
+
+	useEffect(() => {
+		logout();
+		localStorage.removeItem("token");
+		localStorage.removeItem("userId");
+		localStorage.removeItem("genreSelect");
+		setToken(null);
+		nav("/");
+	}, []);
 }
