836a9a8133aab28e996b7fc87cb3755a5889ebab
Emily Cheng <echeng0123@gmail.com>
2024-01-29 15:27:07 -0500

    fixing package json

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
 	plugins: [react()],
 	build: {
 		rollupOptions: {
-			external: [
-				"@mui/material/Rating",
-				"@mui/icons-material/StarBorder",
-				"@mui/material",
-				"@mui/material/Stack",
-			],
+			// external: [
+			// 	"@mui/material/Rating",
+			// 	"@mui/icons-material/StarBorder",
+			// 	"@mui/material",
+			// 	"@mui/material/Stack",
+			// ],
 			outDir: "build",
 		},
 	},
