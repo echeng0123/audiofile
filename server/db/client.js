9bd74e64194190b39393d9f9bd17800246894a3d
Emily Cheng <echeng0123@gmail.com>
2024-01-29 14:34:09 -0500

    updated database for deploy

--- server/db/client.js
+++ server/db/client.js
@@ -3,7 +3,10 @@
 const { Client } = require("pg");
 
 const audio = "audiofile";
-const client = new Client(`postgres://localhost:5432/${audio}`);
+// const client = new Client(`postgres://localhost:5432/${audio}`);
+const client = new Client(
+	`postgres://audiofile_user:YdUwC3IFfc2err9WFoatdE42STHUwhu4@dpg-cmrvn10cmk4c73840t4g-a/audiofile`
+);
 
 // export for use in other files
 module.exports = client;
